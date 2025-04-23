506595c26c27f337662f5433c9381dd7c92a93de
Jorge Lopez Vargas <jorgaf@gmail.com>
2025-04-23 09:32:45 -0500

    Multi-thread implementation

--- src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
+++ src/main/java/ec/edu/utpl/carreras/computacion/proava/MultiTimerConsole.java
@@ -5,8 +5,19 @@ package ec.edu.utpl.carreras.computacion.proava;
  */
 public class MultiTimerConsole {
     public static void main(String[] args) {
-        TimerTask timerA = new TimerTask("Temporizador A", 8);
-        timerA.run();
+        TimerTask timerA = new TimerTask("Temporizador A", 30);
+        Thread thA = new Thread(timerA);
+        thA.start();
+        System.out.println();
+
+        TimerTask timerB = new TimerTask("Temporizador B", 5);
+        Thread thB = new Thread(timerB);
+        thB.start();
+        System.out.println();
+
+        TimerTask timerC = new TimerTask("Temporizador C", 6);
+        Thread thC = new Thread(timerC);
+        thC.start();
         System.out.println();
     }
 }
--- src/main/java/ec/edu/utpl/carreras/computacion/proava/TimerTask.java
+++ src/main/java/ec/edu/utpl/carreras/computacion/proava/TimerTask.java
@@ -1,6 +1,6 @@
 package ec.edu.utpl.carreras.computacion.proava;
 
-public class TimerTask {
+public class TimerTask implements Runnable {
     private final String name;
     private int seconds;
 
@@ -9,9 +9,15 @@ public class TimerTask {
         this.seconds = seconds;
     }
 
+    @Override
     public void run() {
         while(seconds > 0) {
             System.out.printf("%s: %d segundos restantes%n", name, seconds);
+            try {
+                Thread.currentThread().sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.printf("%s ha sido interrumpido%n", name);
+            }
             seconds --;
         }
     }
